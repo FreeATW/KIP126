9008a098282d3782488e1a50d1cd85107da2b573
surenny <surenny233@sjtu.edu.cn>
2026-09-20 08:12:45 +0000

    limit challenge entrypoint to milestones

--- KIP126/Challenge.lean
+++ KIP126/Challenge.lean
@@ -1,10 +1,4 @@
-import KIP126.Challenge.Tools.Comparison.Proof
 import KIP126.Challenge.Near126.Thm7_3BJMBX.Statement
-import KIP126.Challenge.Tools.Thm6_1Leibniz.Statement
-import KIP126.Challenge.Tools.Thm6_12Mahowald.Statement
-import KIP126.Challenge.Tools.PagePropagation.Statement
-import KIP126.Challenge.Tools.FilteredComplexRelations.Statement
-import KIP126.Challenge.Tools.StableHomotopy.Statement
 import KIP126.Challenge.Near126.OnlyD12.Statement
 import KIP126.Challenge.Near126.CandidateReduction.Statement
 import KIP126.Challenge.Near126.Conditions.Statement
